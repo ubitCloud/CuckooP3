6ff267f2f9b3523978d07f9e94ee4d3cce908af6
Ubit Cloud <ubitcloud2@gmail.com>
2024-07-23 12:22:24 +0500

    update cuckoofile.toml + some changes.

--- CuckooP3/ContentView.swift
+++ CuckooP3/ContentView.swift
@@ -7,18 +7,23 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
-
-#Preview {
-    ContentView()
-}
+//@available(iOS 15.0, *)
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+//    }
+//}
+//
+//#Preview {
+//    if #available(iOS 15.0, *) {
+//        ContentView()
+//    } else {
+//        // Fallback on earlier versions
+//    }
+//}
